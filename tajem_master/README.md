c68342211cc1dd19a8c3c9c051094b5184a27de6
maria1186 <rangelova1186@gmail.com>
2019-06-01 12:50:02 +0300

    Tajem master update files

---
+++ tajem_master/README.md
@@ -0,0 +1,10 @@
+# This is refactoring of old code  
+
+## Changes
+
+- fix responsive bugs
+- write separate js file for javascript
+- instead of 10 logos of famous brands created one .gif image
+- compress images
+- add button back to top  
+- add README markdown file
---
+++ tajem_master/custom.js
@@ -0,0 +1,54 @@
+//Bootstrap scrollspy animate
+
+$(function(){
+  // Add scrollspy to <body>
+  $('body').scrollspy({target: ".navbar", offset: 50});   
+
+  // Add smooth scrolling on all links inside the navbar
+  $("#navbar-collapse a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+   
+});
+
+$(function() {
+   //Set the carousel options
+    $('#quote-carousel').carousel({
+      pause: true,
+      interval: 4000,
+    });
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollBtn").style.display = "block";
+  } else {
+    document.getElementById("scrollBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
